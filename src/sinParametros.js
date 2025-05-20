602eb1e6e36552381c62e39a3812a43743a4fa90
Sebastian Ospina <asebasg07@gmail.com>
2025-05-20 15:40:42 -0500

    Se eliminaron los archivos de los ejercicios individuales y se integraron en la rama principal en un solo archivo llamado "sebastian.js"

--- src/sinParametros.js
+++
@@ -1,9 +0,0 @@
-/**
- * Crea una arrow function sin parámetros que devuelva un mensaje fijo.
- */
-
-function mensaje() {
-    return `Hola, este es un simple mensaje...`
-}
-
-console.log(mensaje());
--- src/sumaNumeros.js
+++
@@ -1,9 +0,0 @@
-/**
- * Crea una expresión de función que sume dos números y devuelva el resultado.
- */
-
-function suma(a, b) {
-  return a + b;
-}
-
-console.log(suma(3, 5));
